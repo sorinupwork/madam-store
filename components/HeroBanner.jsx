0640ad1b9eea06e2fb84545c8103f83b35449c10
sorinupwork <sorin.upwork@gmail.com>
2022-12-05 23:33:29 +0200

    add metaTags

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
@@ -10,11 +11,14 @@ const HeroBanner = ({ heroBanner }) => {
         <p className="beats-solo">{heroBanner.smallText}</p>
         <h3>{heroBanner.midText}</h3>
         <h1>{heroBanner.largeText1}</h1>
-        <img
-          src={urlFor(heroBanner.image)}
-          alt="headphones"
-          className="hero-banner-image"
-        />
+        {urlFor(heroBanner.image) > 0 && (
+          <Image
+            src={urlFor(heroBanner.image)}
+            alt="headphones"
+            className="hero-banner-image"
+          />
+        )}
+
         <div>
           <Link href={`/product/${heroBanner.product}`}>
             <button type="button">{heroBanner.buttonText}</button>
--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,47 @@ import { Layout } from "../components";
 import "../styles/globals.css";
 import { StateContext } from "../context/StateContext";
 import { Toaster } from "react-hot-toast";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <StateContext>
       <Layout>
+        <Head>
+          <title>MADAM Store</title>
+          <meta name="title" content="MADAM Store" />
+          <meta
+            name="description"
+            content="What is MADAM Store? Ecommerce shop, also known as electronic commerce or internet commerce, refers to the buying and selling of goods or services using the internet, and the transfer of money and data to execute these transactions."
+          />
+
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://madam-store.vercel.app/" />
+          <meta property="og:title" content="MADAM Store" />
+          <meta
+            property="og:description"
+            content="What is MADAM Store? Ecommerce shop, also known as electronic commerce or internet commerce, refers to the buying and selling of goods or services using the internet, and the transfer of money and data to execute these transactions."
+          />
+          <meta
+            property="og:image"
+            content="https://i.ibb.co/tXHkQxf/madam-logo2.png"
+          />
+
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta
+            property="twitter:url"
+            content="https://madam-store.vercel.app/"
+          />
+          <meta property="twitter:title" content="MADAM Store" />
+          <meta
+            property="twitter:description"
+            content="What is MADAM Store? Ecommerce shop, also known as electronic commerce or internet commerce, refers to the buying and selling of goods or services using the internet, and the transfer of money and data to execute these transactions."
+          />
+          <meta
+            property="twitter:image"
+            content="https://i.ibb.co/tXHkQxf/madam-logo2.png"
+          />
+        </Head>
         <Toaster />
         <Component {...pageProps} />
       </Layout>
