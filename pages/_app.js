f956f8d2c8278a4e65236f80d2e929c3b3f3685e
sorinupwork <sorin.upwork@gmail.com>
2022-12-05 23:48:17 +0200

    fix images typo

--- pages/_app.js
+++ pages/_app.js
@@ -16,23 +16,23 @@ function MyApp({ Component, pageProps }) {
             content="What is MADAM Store? Ecommerce shop, also known as electronic commerce or internet commerce, refers to the buying and selling of goods or services using the internet, and the transfer of money and data to execute these transactions."
           />
 
-          <link rel="shortcut icon" href="/images/favicon.ico" />
+          <link rel="shortcut icon" href="./images/favicon.ico" />
           <link
             rel="apple-touch-icon"
             sizes="180x180"
-            href="/images/apple-touch-icon.png"
+            href="./images/apple-touch-icon.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="32x32"
-            href="/images/favicon-32x32.png"
+            href="./images/favicon-32x32.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="16x16"
-            href="/images/favicon-16x16.png"
+            href="./images/favicon-16x16.png"
           />
 
           <meta property="og:type" content="website" />
